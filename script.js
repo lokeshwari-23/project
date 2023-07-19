22460e1274f38fd90d6f8e5746e14c6c9d3ca344
lokeshwari-23 <134676662+lokeshwari-23@users.noreply.github.com>
2023-07-19 13:27:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let addtask=document.getElementById("btn")
+let form=document.getElementById("popup")
+let submit=document.getElementById("btn1");
+let val=document.getElementById("myInputField").value;
+
+
+addtask.addEventListener("click", function() {
+    console.log("add eventlistener clicked")
+    form.style.display="grid";
+    form.style.justifyContent="center";
+
+
+ })
+//  btn1.addEventListener("click", (event)=> {
+//     event.preventDefault();
+//     console.log("title clicked")
+
+//     let arr=[];
+//     console.log(arr);
+//     document.getElementById("title").value;
+//  })
+
+btn1.addEventListener("click", (event)=>{
+    let val=document.getElementById("myInputField").value;
+    console.log(val);
+   
+    let arr=[];
+    arr.push(val);
+    let content=document.getElementById("content");
+
+
+   
+    // event.preventDefault();
+
+    // const inputFields = document.querySelector(".myForm input[type='text']");
+    // const inputValues = [];
+    
+    //   inputValues.push(inputFields.value);
+    
+    // console.log(inputValues);
+  })
+ const openPopup = () => {
+  popup.style.display = "block";
+  popupbg.style.display = "block";
+ };
+ 
+ const closePopup = () => {
+  popup.style.display = "none";
+  popupbg.style.display = "none";
+ };
